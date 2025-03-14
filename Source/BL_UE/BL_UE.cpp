48d4f84e964fe34e848fc3306188ce06db21acf6
JTMurray <60299252+JTMurrayTAH@users.noreply.github.com>
2025-03-14 14:46:43 +0000

    Initialisation of Project
    Setting files into position. Clean Project, no additional files.

---
+++ Source/BL_UE/BL_UE.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BL_UE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BL_UE, "BL_UE" );
